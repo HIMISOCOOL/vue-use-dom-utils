cfb32afd5bcea4f627989f86c3bb2139602f090b
Matthew Meehan <matusmarx@hotmail.com>
2021-02-20 17:08:32 +1300

    updated to use rollup for build instead of vue cli building types flat into dist. targeting es2015 since thats the min version of the internet we wanna support. updated package.json with a description, some tags and the repo link.

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    chainWebpack: config => {
-        config.externals({
-            vue: 'vue',
-            '@vue/composition-api': '@vue/composition-api'
-        });
-    }
-};
